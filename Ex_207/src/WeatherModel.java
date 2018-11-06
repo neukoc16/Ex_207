b03268c0b3e9f2ba8b70eacc1590a49a34bb46fa
neukoc16 <neukoc16@new.edvhtl.at>
2018-11-06 09:42:57 +0100

    minor changes

--- Ex_207/src/WeatherModel.java
+++ Ex_207/src/WeatherModel.java
@@ -5,7 +5,7 @@ import javax.swing.table.AbstractTableModel;
 public class WeatherModel extends AbstractTableModel {
 
     private ArrayList<Weatherstation> stations = new ArrayList<>();
-    private String[] coNames = {"Place", "Sea Level", "Temperature", "rel. Humidity"};
+    private String[] colNames = {"Place", "Sea Level", "Temperature", "rel. Humidity"};
 
     @Override
     public int getRowCount() {
@@ -20,6 +20,11 @@ public class WeatherModel extends AbstractTableModel {
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
 
+    }
+
+    @Override
+    public String getColumnName(int i) {
+        return colNames[i];
     }
 
 }
--- Ex_207/src/Weatherstation.java
+++ Ex_207/src/Weatherstation.java
@@ -4,13 +4,13 @@ public class Weatherstation {
     private String place;
     private int sealevel;
     private double temperature;
-    private int huidity;
+    private int humidity;
 
-    public Weatherstation(String place, int sealevel, double temperature, int huidity) {
+    public Weatherstation(String place, int sealevel, double temperature, int humidity) {
         this.place = place;
         this.sealevel = sealevel;
         this.temperature = temperature;
-        this.huidity = huidity;
+        this.humidity = humidity;
     }
 
     public double getTemperature() {
@@ -26,7 +26,23 @@ public class Weatherstation {
     }
 
     public int getHuidity() {
-        return huidity;
+        return humidity;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    public void setSealevel(int sealevel) {
+        this.sealevel = sealevel;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
     }
 
 }
